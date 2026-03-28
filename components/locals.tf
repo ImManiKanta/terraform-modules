bece12fb73ec97f1062d022b656a9f9c1eb2c6ec
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-28 10:13:02 +0530

    locals updated

--- components/locals.tf
+++ components/locals.tf
@@ -1,7 +1,6 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
-  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   sg_id = data.aws_ssm_parameter.sg_id.value
